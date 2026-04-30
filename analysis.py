ca512387c6d22a43abcc03537f1efc3871437699
Angelica B. <xcb9@txstate.edu>
2026-04-30 18:48:11 -0500

    added barchart for materials for libraries

--- analysis.py
+++ analysis.py
@@ -4,19 +4,44 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-#st.title("Library Analysis")
+st.title("Library Analysis")
 df = pd.read_csv("PLS_FY23_AE_pud23i.csv",encoding="cp1252")
-#st.dataframe(df)
-#Libraries = st.selectbox(
-#"Libraries", df["BRANLIB"].unique())
-#st.bar_chart
-#st.write(df) # DataFrame
-#st.write(fig) # Plotly / Altair fig
-#st.write({"a":1}) # JSON
-#st.write(model)
 
+df.columns = df.columns.str.strip()
+df = df.dropna(subset=["POPU_LSA", "BKVOL"])
+
+with st.sidebar:
+    state = st.selectbox(
+        "Select State",
+        sorted(df["STABR"].dropna().unique())
+    )
+
+    state_df = df[df["STABR"] == state]
+
+    library = st.selectbox(
+    
+        "LIBNAME",
+        sorted(state_df["LIBNAME"].dropna().unique())
+    )
+
+# ✅ Filter data
+
+lib_df = state_df[state_df["LIBNAME"] == library]
+
+st.write(f"### Population vs Book Volume in {state}")
 st.write("Population vs Book Volume vs Library Branches in a City")
-fig = px.scatter(df, x="POPU_LSA",
+fig = px.scatter(state_df, x="POPU_LSA",
 y="BKVOL", color="BRANLIB")
-st.write(df.columns)
+st.plotly_chart(fig, use_container_width=True)
+
+materials = lib_df[["PRMATEXP", "ELMATEXP"]].sum().reset_index()
+materials.columns = ["Material Type", "Expenditure"]
+st.write(f"### Materials in {library}")
+st.write("Material types in library")
+fig = px.bar(
+    materials,
+    x="Material Type",
+    y="Expenditure",
+    title=f"Material Expenditures in {library}"
+)
 st.plotly_chart(fig, use_container_width=True)
